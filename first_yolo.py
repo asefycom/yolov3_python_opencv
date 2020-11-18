76c722857255e5e161ebbf71064292c9158cbfb7
mostafa-asefy <asefi.v@gmail.com>
2020-11-18 09:37:22 +0330

    L03 - COCO Names

--- first_yolo.py
+++ first_yolo.py
@@ -3,6 +3,15 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+coco_file = "coco.names"
+coco_classes = []
+
+with open(coco_file, "rt") as f:
+    coco_classes = f.read().rstrip("\n").split("\n")
+
+print(coco_classes)
+print(len(coco_classes))
+
 while True:
     success, frame = cap.read()
 
