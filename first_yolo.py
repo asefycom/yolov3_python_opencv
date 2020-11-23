6f6a14d48cc5b9c038fab3342ca9844976dd32b0
mostafa-asefy <asefi.v@gmail.com>
2020-11-23 14:02:27 +0330

    L05-06 - Read DarkNet

--- first_yolo.py
+++ first_yolo.py
@@ -5,12 +5,18 @@ cap = cv2.VideoCapture(0)
 
 coco_file = "coco.names"
 coco_classes = []
+net_config = "cfg/yolov3.cfg"
+net_weights = "cfg/yolov3.weights"
 
 with open(coco_file, "rt") as f:
     coco_classes = f.read().rstrip("\n").split("\n")
 
 print(coco_classes)
 print(len(coco_classes))
+
+net = cv2.dnn.readNetFromDarknet(net_config, net_weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 while True:
     success, frame = cap.read()
