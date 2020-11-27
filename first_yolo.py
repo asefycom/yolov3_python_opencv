16a03dd52ca31f51fc3927869482da249abb5795
mostafa-asefy <asefi.v@gmail.com>
2020-11-27 12:28:38 +0330

    L09 - Show Objects

--- first_yolo.py
+++ first_yolo.py
@@ -8,6 +8,8 @@ coco_classes = []
 net_config = "cfg/yolov3.cfg"
 net_weights = "cfg/yolov3.weights"
 blob_size = 320
+confidence_threshold = 0.5
+nms_threshold = 0.3
 
 with open(coco_file, "rt") as f:
     coco_classes = f.read().rstrip("\n").split("\n")
@@ -18,6 +20,33 @@ with open(coco_file, "rt") as f:
 net = cv2.dnn.readNetFromDarknet(net_config, net_weights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+def findObjects(output, img):
+    img_h, img_w, img_c = img.shape
+    bboxes = []
+    class_ids = []
+    confidences = []
+
+    for cell in output:
+        for detect_vector in cell:
+            scores = detect_vector[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > confidence_threshold:
+                w,h = int(detect_vector[2] * img_w), int(detect_vector[3] * img_h)
+                x,y = int((detect_vector[0] * img_w) - w/2), int((detect_vector[1] * img_h) - h/2)
+                bboxes.append([x,y,w,h])
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+    indices = cv2.dnn.NMSBoxes(bboxes, confidences, confidence_threshold, nms_threshold)
+    print(indices)
+    for i in indices:
+        i = i[0]
+        bbox = bboxes[i]
+        x,y,w,h = bbox[0], bbox[1], bbox[2], bbox[3]
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+        cv2.putText(img, f'{coco_classes[class_ids[i]].upper()} {int(confidences[i] * 100)}%',
+                    (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
 
 while True:
     success, frame = cap.read()
@@ -35,6 +64,7 @@ while True:
     # print(output[0].shape)
     # print(output[1].shape)
     # print(output[2].shape)
+    findObjects(output, frame)
 
 
     cv2.imshow("Webcam", frame)
